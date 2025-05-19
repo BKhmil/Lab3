cbbedc0a55544606f51cb7630409bd2c300013d2
Bohdan Khmil <khmil.work@gmail.com>
2025-05-19 19:12:47 +0300

    Add project files.

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numericUpDown_depth = new NumericUpDown();
+            button_start = new Button();
+            richTextBox_output = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_depth).BeginInit();
+            SuspendLayout();
+            // 
+            // numericUpDown_depth
+            // 
+            numericUpDown_depth.Location = new Point(12, 12);
+            numericUpDown_depth.Name = "numericUpDown_depth";
+            numericUpDown_depth.Size = new Size(120, 23);
+            numericUpDown_depth.TabIndex = 0;
+            // 
+            // button_start
+            // 
+            button_start.Location = new Point(188, 12);
+            button_start.Name = "button_start";
+            button_start.Size = new Size(89, 33);
+            button_start.TabIndex = 1;
+            button_start.Text = "start";
+            button_start.UseVisualStyleBackColor = true;
+            button_start.Click += start_Click;
+            // 
+            // richTextBox_output
+            // 
+            richTextBox_output.Location = new Point(12, 72);
+            richTextBox_output.Name = "richTextBox_output";
+            richTextBox_output.Size = new Size(776, 366);
+            richTextBox_output.TabIndex = 2;
+            richTextBox_output.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox_output);
+            Controls.Add(button_start);
+            Controls.Add(numericUpDown_depth);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_depth).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown_depth;
+        private Button button_start;
+        private RichTextBox richTextBox_output;
+    }
+}
---
+++ Lab3/Form1.cs
@@ -0,0 +1,90 @@
+﻿namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        //        поточне число Фібоначчі  сума всіх згенерованих чисел
+        private long currentFibonacci = 0, sum = 0;
+
+        // семафор для синхронізації потоків: один чекає, поки інший згенерує число
+        // за замовчуванням доступу до нього немає для жодного потоку
+        // максимум потоків, які будуть отримувати доступ, встановив в 1
+        private Semaphore semaphore = new Semaphore(0, 1);
+
+        // глибина — скільки чисел Фібоначчі буде згенеровано
+        private int depth;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // обмеження глибини для коректної роботи
+            this.numericUpDown_depth.Minimum = 3;
+            this.numericUpDown_depth.Maximum = 15;
+        }
+
+        // обробник натискання кнопки "start"
+        // в ньому роблю кнопку "start" неактивною, отримую значення для depth з NumericUpDown
+        // очищую RichTextBox, скидую суму та запускаю метод Run()
+        private void start_Click(object sender, EventArgs e)
+        {
+            this.button_start.Enabled = false;
+
+            depth = (int)numericUpDown_depth.Value;
+            richTextBox_output.Text = "";
+            sum = 0;
+
+            this.Run();
+        }
+
+        // Безпосередньо метод Run(), в ньому просто запускаю потоки зі встановленим
+        // значенням для Thread.IsBackground як true
+        // кожен з потоків виконує свій метод для обчислень
+        private void Run()
+        {
+            Thread th1 = new Thread(this.Th1Task);
+            Thread th2 = new Thread(this.Th2Task);
+
+            th1.IsBackground = true;
+            th2.IsBackground = true;
+
+            th1.Start();
+            th2.Start();
+        }
+
+        // метод який просто генерує числа Фібоначчі
+        // після генерації числа даю дозвіл на виконання для другого потоку
+        // для наочності зробив затримку на секунду
+        private void Th1Task()
+        {
+            long a = 0, b = 1;
+            for (int i = 0; i < this.depth; ++i)
+            {
+                this.currentFibonacci = a;
+                this.semaphore.Release();
+
+                Thread.Sleep(1000);
+
+                long next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+
+        // метод що очікую на генерацію числа, після чого додає його до загальної суми та виводить в UI
+        // Invoke()'и для доступу до елементів основного потоку з дочірніх потоків
+        private void Th2Task()
+        {
+            for (int i = 0; i < this.depth; i++)
+            {
+                this.semaphore.WaitOne();
+
+                this.sum += currentFibonacci;
+
+                this.Invoke(() => richTextBox_output.AppendText(
+                    $"Fibonacci num: {currentFibonacci}, Sum: {sum}\r\n"));
+            }
+
+            this.Invoke(() => button_start.Enabled = true);
+        }
+    }
+}
